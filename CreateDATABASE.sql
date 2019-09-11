0d948ecadc243716876346146d835ee28fdb4d8b
da3210 <da3210@gmail.com>
2019-09-12 00:28:05 +0200

    changed to datetime, added View to Grafana

--- CreateDATABASE.sql
+++ CreateDATABASE.sql
@@ -11,7 +11,7 @@ CREATE TABLE [dbo].[devices](
 
 CREATE TABLE [dbo].[History](
 	[periph_id] [nvarchar](50) NOT NULL,
-	[date] datetime2 NOT NULL,
+	[date] [datetime] NOT NULL,
 	[value] [nvarchar](50) NULL,
  CONSTRAINT [PK_History] PRIMARY KEY CLUSTERED 
 (
@@ -19,4 +19,12 @@ CREATE TABLE [dbo].[History](
 	[date] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
+GO
+
+
+CREATE VIEW [dbo].[v_Grafana]
+AS
+SELECT        dbo.History.date AS time, CASE WHEN ISNUMERIC(value) = 1 THEN CONVERT(float, value) ELSE NULL END AS Value, dbo.devices.name
+FROM            dbo.History INNER JOIN
+                         dbo.devices ON dbo.History.periph_id = dbo.devices.periph_id
 GO
